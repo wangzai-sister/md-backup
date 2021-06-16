202b5762b4d869b86f5462b6e418141e9eeab5c9
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-06-16 12:50:38 +0800

    Update warp4.sh

--- warp4.sh
+++ warp4.sh
@@ -26,4 +26,4 @@ systemctl start wg-quick@wgcf
 rm -f warp4* wgcf* wireguard-go*
 grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
 echo -e "\033[1;33m 检测是否成功启动Warp！\n 显示IPV4地址：$(wget -qO- ipv4.ip.sb) \033[0m"
-echo -e "\033[1;32m 如上方显示IPV4地址：8.…………，则说明成功啦！\n 如上方无IP显示,（说明申请WGCF账户失败），请“无限”重复运行该脚本吧，直到成功为止！！！ \033[0m"
+echo -e "\033[1;32m 如上方显示IPV4地址：8.…………，则说明成功啦！\n 如上方无IP显示,（说明申请WGCF账户失败），请重复运行该脚本吧，直到成功为止！！！ \033[0m"
