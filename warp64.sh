51a260887a2900a96508044468ab5f2b4e94e929
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-07-06 21:15:36 +0800

    Update warp64.sh

--- warp64.sh
+++ warp64.sh
@@ -16,8 +16,8 @@ sleep 1s
 echo | wgcf register
 done
 wgcf generate
-sed -i "5 s/^/PostUp = ip -6 rule add from $(wget -qO- ipv6.ip.sb) table main\n/" wgcf-profile.conf
-sed -i "6 s/^/PostDown = ip -6 rule delete from $(wget -qO- ipv6.ip.sb) table main\n/" wgcf-profile.conf
+sed -i "5 s/^/PostUp = ip -6 rule add from $(wget -qO- -6 ip.gs) table main\n/" wgcf-profile.conf
+sed -i "6 s/^/PostDown = ip -6 rule delete from $(wget -qO- -6.ip.gs) table main\n/" wgcf-profile.conf
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
 cp wgcf-account.toml /etc/wireguard/wgcf-account.toml
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
@@ -25,5 +25,5 @@ systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
 rm -f warp64* wgcf* wireguard-go*
 grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
-echo -e "\033[1;33m 检测是否成功启动（IPV4+IPV6）双栈Warp！\n 显示IPV4地址：$(wget -qO- ipv4.ip.sb) 显示IPV6地址：$(wget -qO- ipv6.ip.sb) \033[0m"
+echo -e "\033[1;33m 检测是否成功启动（IPV4+IPV6）双栈Warp！\n 显示IPV4地址：$(wget -qO- -4.ip.gs) 显示IPV6地址：$(wget -qO- -6.ip.gs) \033[0m"
 echo -e "\033[1;32m 如上方显示IPV4地址：8.…………，IPV6地址：2a09:…………，则说明成功啦！\n 如上方IPV4无IP显示,IPV6显示本地IP（说明申请WGCF账户失败），请重复运行该脚本吧，直到成功为止！！！ \033[0m"
